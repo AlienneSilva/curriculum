512e5eaafb519b9cf8f88fbd928e9cb2b7933ed2
AlienneSilva <88863077+AlienneSilva@users.noreply.github.com>
2026-01-27 17:11:02 -0300

    Atualiza o codigo

--- assets/js/api.js
+++
@@ -1,5 +0,0 @@
-async function fetchProfileData(){
-    const url ='https://github.com/AlienneSilva/curriculum/tree/main/data/profile.json'
-    const fetching = await fecth(url)
-    return await fetching.json()
-}
